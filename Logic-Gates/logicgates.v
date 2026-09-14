204524cbf64ec16b37c1309b1ad130ec05695115
Thavasrinithi k <thavasrinithikarthi@gmail.com>
2026-09-14 22:34:07 +0530

    Add logic gates Verilog design

---
+++ Logic-Gates/logicgates.v
@@ -0,0 +1,11 @@
+    module logicgates (A,B,Yand,Yor,Ynand,Ynor,Ynot,Yxnor,Yxor)
+    input A,B;
+    output Yand,Yor,Ynand,Ynor,Ynot,Yxnor,Yxor;
+    and  (Yand,  A, B);
+    or   (Yor,   A, B);
+    nand (Ynand, A, B);
+    nor  (Ynor,  A, B);
+    not  (Ynot,  A);
+    xnor (Yxnor, A, B);
+    xor  (Yxor,  A, B);
+    endmodule
